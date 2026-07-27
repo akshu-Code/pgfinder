09ed0a430f70b9d55a151b1c86dc5e06f51c5581
akshu-Code <akshitrana8860221321@gmail.com>
2026-07-27 22:40:58 +0530

    Add vercel configuration

--- app.py
+++ app.py
@@ -79,9 +79,6 @@ def signup():
 def thankyou():
     return render_template("thankyou.html")
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port)
 
 if __name__ == "__main__":
     app.run(debug=True)
